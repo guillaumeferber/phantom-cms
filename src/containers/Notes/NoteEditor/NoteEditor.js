9cf5a40aa12c21f5ee37cf377b71f2a303ce4036
Guillaume Ferber <guillaume@agenceminimal.com>
2020-05-22 16:09:07 -0400

    Install CKEditor

--- src/containers/Notes/NoteEditor/NoteEditor.js
+++ src/containers/Notes/NoteEditor/NoteEditor.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux/Aux';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import * as cx from './NoteEditor.css';
 
 const noteEditor = (props) => {
@@ -9,6 +11,24 @@ const noteEditor = (props) => {
             <div className={cx.NoteEditor}>
                 <h2>Note Editor</h2>
                 {props.note ? <p>{props.note.name + ' #' + props.note.id}</p> : null}
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onInit={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
             </div>
         </Aux>
      );
